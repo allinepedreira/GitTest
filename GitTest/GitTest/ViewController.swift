be8e9d62661cb4885039aa90730756a07936b811
Alline Pedreira <allinepedreira@Allines-MacBook-Pro.local>
2017-01-18 15:19:49 -0200

    Commit oi

--- GitTest/GitTest/ViewController.swift
+++ GitTest/GitTest/ViewController.swift
@@ -12,9 +12,11 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var image: UIImageView!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("Oi")
     }
 
     override func didReceiveMemoryWarning() {
