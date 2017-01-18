e448aeae819ff4df9c379bff757cd7af30872387
Alline Pedreira <allinepedreira@Allines-MacBook-Pro.local>
2017-01-18 15:36:35 -0200

    Imagem

--- GitTest/GitTest/ViewController.swift
+++ GitTest/GitTest/ViewController.swift
@@ -18,7 +18,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         print("Olá Alline")
         image.image = UIImage(named: "Screen")
-        image.frame.height = CGFloat(50)
+        image.image = UIImage(named: "Screen")
     }
 
     override func didReceiveMemoryWarning() {
